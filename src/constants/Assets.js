c7ef0894d682748ca773d62ffbbaf2efe75a528e
Hieu Khuong <khuonghuyhieu0705@gmail.com>
2023-08-19 23:51:47 +0700

    feat: setup font family

--- src/constants/Assets.js
+++ src/constants/Assets.js
@@ -3,6 +3,7 @@ export default {
   COMMON: {
     menu: "/icons/icon_menu.svg",
     logo: "/icons/logo_black.svg",
+    logo_white: "/icons/logo_white.svg",
     arrow: "/icons/icon_arrow.svg",
     login: "/icons/icon_login.svg",
   },
@@ -11,5 +12,12 @@ export default {
     active_right: "/icons/nav-active-right.svg",
     left: "/icons/nav-left.svg",
     right: "/icons/nav-right.svg",
+  },
+  FOOTER: {
+    facebook: "/icons/social_fb.svg",
+    twitter: "/icons/social_tw.svg",
+    youtube: "/icons/social_yt.svg",
+    google: "/icons/social_gl.svg",
+    linkedin: "/icons/social_in.svg",
   },
 };
--- src/constants/Types.js
+++ src/constants/Types.js
@@ -5,3 +5,38 @@ export const Menu = {
   TOOLS: "Tools",
   PRICING: "Pricing",
 };
+
+export const MenuFooter = {
+  COURSE: {
+    TITLE: "Course",
+    CHILDREN: {
+      INFO: "Info course",
+      PROGRAM: "Program",
+      TOOLS: "tools",
+    },
+  },
+  SOCIAL: {
+    TITLE: "Social",
+    CHILDREN: {
+      FB: "Facebook",
+      TW: "Twitter",
+      ITG: "Instagram",
+    },
+  },
+  PLANS_PRICING: {
+    TITLE: "Plans & Pricing",
+    CHILDREN: {
+      PRICING: "Pricing",
+      TEAM: "Team",
+      BUSINESS: "Business",
+    },
+  },
+  CONTACTS: {
+    TITLE: "Contacts",
+    CHILDREN: {
+      ABOUT: "About us",
+      HOW: "How it works",
+      PRICING: "Pricing",
+    },
+  },
+};
